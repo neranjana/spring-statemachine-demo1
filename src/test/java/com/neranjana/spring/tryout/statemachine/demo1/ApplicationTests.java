3fc7733d8e05b4f5405133774fc362ed42d2c139
neranjana <neranjana@gmail.com>
2021-09-26 12:18:30 +1000

    Adding Maven files etc.

---
+++ src/test/java/com/neranjana/spring/tryout/statemachine/demo1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neranjana.spring.tryout.statemachine.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
